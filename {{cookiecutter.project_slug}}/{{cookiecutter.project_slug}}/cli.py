503a1ccfeb4dd590214ef891edf396ce500a83f4
userusr <userusr@yandex.ru>
2020-05-10 15:23:57 +0300

    Add default logging to stdout

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -1,17 +1,48 @@
 """Console script for {{cookiecutter.project_slug}}."""
-
+import logging
+import logging.config
+import sys
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
 import argparse
 {%- endif %}
-import sys
 {% if cookiecutter.command_line_interface|lower == 'click' %}
 import click
 {%- endif %}
 
+from {{ cookiecutter.project_slug }}.{{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug.replace('_', ' ').title().replace(' ', '') }}
+
+def _init_logging(verbose: int) -> None:
+    level = logging.ERROR
+
+    logger = logging.getLogger()
+    handler = logging.StreamHandler(stream=sys.stdout)
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    if verbose == 1:
+        level = logging.WARNING
+    elif verbose == 2:
+        level = logging.INFO
+    elif verbose > 2:
+        level = logging.DEBUG
+
+    handler.setLevel(level)
+    logger.setLevel(level)
+
+
 {% if cookiecutter.command_line_interface|lower == 'click' %}
 @click.command()
-def main(args=None):
+@click.option("-v", "--verbose", default=1, count=True)
+def main(verbose: int, args=None) -> None:
     """Console script for {{cookiecutter.project_slug}}."""
+    _init_logging(verbose)
+    logger = logging.getLogger("cli.main")
+
+    logger.debug("Logger initialized")
+
+    {{ cookiecutter.project_slug }} = {{ cookiecutter.project_slug.replace('_', ' ').title().replace(' ', '') }}()  # noqa: F841
+
     click.echo("Replace this message by putting your code into "
                "{{cookiecutter.project_slug}}.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -1,1 +1,11 @@
 """Main module."""
+import logging
+
+
+class {{ cookiecutter.project_slug.replace('_', ' ').title().replace(' ', '') }}():
+    """Class for {{ cookiecutter.project_slug }}."""
+
+    def __init__(self) -> None:
+        """Constract {{ cookiecutter.project_slug }}."""
+        self.logger = logging.getLogger("{{ cookiecutter.project_slug }}")
+        self.logger.debug("{{ cookiecutter.project_slug.replace('_', ' ').title().replace(' ', '') }} initialized")
